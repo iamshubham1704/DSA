ed577802261b7c80b87abdd5fe6180e314b53277
iamshubham1704 <sstech514@gmail.com>
2026-02-27 13:15:25 +0530

    commit

---
+++ kadane_algo/kadane_algo.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        long long max = LLONG_MIN;
+        long long sum = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+
+            if (sum > max)
+            {
+                max = sum;
+            }
+
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+};
---
+++ rearrange_array/rearrange_array.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos;
+        vector<int> neg;
+
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i]>0){
+                pos.push_back(nums[i]);
+            } else{
+                neg.push_back(nums[i]);
+            }
+        }
+
+        for(int i = 0; i<nums.size()/2;i++){
+            nums[2 * i] = pos[i];
+            nums[2 * i + 1] = neg[i];
+        }
+
+        return nums;
+    }
+};
